81723be8b4983957d2e549f04e24b0e3fe2c8a76
Abdullah <abdullah.s.nuaimi@gmail.com>
2025-04-20 22:11:13 +0300

    recreating the profile page

--- frontend/src/pages/Dashboard/dashboard.page.jsx
+++ frontend/src/pages/Dashboard/dashboard.page.jsx
@@ -1,20 +1,11 @@
 import "./dashboard.css"
-import UniversityDropdowns from "../../components/EducationSelection/educationSelection.component";
-import EditUserInfo from "../../components/EditUserInfo/editUserInfo.component";
-import ProfileImageUploader from "../../components/profileImageUploader/profileImageUploader.component";
-import CareerEditor from "../../components/CareerEditor/careerEditor.component";
-import PasswordUpdater from "../../components/PasswordUpdater/passwordUpdater.component";
+
 
 const Dashboard = () => {
 
   return (
     <div className="dashboard-container">
       <h1>Dashbord page</h1>
-      <UniversityDropdowns />
-      <EditUserInfo />
-      <ProfileImageUploader />
-      <CareerEditor />
-      <PasswordUpdater />
     </div> 
   );
 };
